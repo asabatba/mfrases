d3419d8aa49c7c4420bd6ddfc4ecfb41c4d9c08d
Arnau <kenny.knp@gmail.com>
2019-09-12 01:47:03 +0200

    output

--- preproc.py
+++ preproc.py
@@ -1,5 +1,6 @@
 import sqlite3
 import re
+import csv
 
 litecon = sqlite3.connect('mfrases.db')
 cursor = litecon.cursor()
@@ -9,6 +10,8 @@ english_words = ['the', 'of', 'and', 'that',
 
 spanish_words = ['que', 'los', 'se', 'por', 'una', 'uno',
                  'su', 'para', 'como', 'sobre', 'entre', 'porque']
+
+word_dict = {}
 
 for (idx, row) in enumerate(cursor.execute("SELECT quote_text FROM wikiquote")):
     # print("{}. {}".format(idx, row[0]))
@@ -22,13 +25,30 @@ for (idx, row) in enumerate(cursor.execute("SELECT quote_text FROM wikiquote")):
     # print(oraciones)
     for o in oraciones:
         # o = [x for x in o.strip().lower() if re.match("[\w]+", x)]
-        palabras = o.split(" ")
+        palabras = re.split("[ (,)(;)(:)]", o)  # o.split(" ")
+
         is_english = any(item in palabras for item in english_words)
         is_spanish = any(item in palabras for item in spanish_words)
         # if (is_english):
         # print("-> en={}\tes={}\t: {}".format(is_english, is_spanish, o))
         if (is_spanish):
-            print(o)
+            # print(o)
+            for word in palabras:
+                word = re.sub("[^\w]", "", word)
+                if re.match("[\w]+", word) == None:
+                    continue
+                if word in word_dict:
+                    word_dict[word] += 1
+                else:
+                    word_dict[word] = 1
 
-    if idx == 100:
+    if idx == 100000:
         break
+
+
+with open('wordcount.csv', 'w', newline='', encoding='UTF-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['word', 'frequency'])
+
+    for row in word_dict:
+        writer.writerow([row, word_dict[row]])
